84596fbace5c2efa60fafce8c2e56f9daa816140
Samuel Alleaume <samuel@alleaume.eu>
2023-11-07 13:53:10 +0100

    :fire: Adding boxes and dealing with the API first

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,12 @@
+import "./nav.css"
+import WIP from '../../assets/WIP.png'
+
+function Nav() {
+            <div className='inner-container'>
+              <img src={WIP} alt="WIP"/>
+              <p>test</p>
+                
+            </div>
+}
+
+export default Nav
